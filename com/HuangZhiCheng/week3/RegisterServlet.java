5b5e60f587682648725dc1d5a0facce280f9ea19
2019211001001120-HuangZhiCheng <80087154+2019211001001120-HuangZhiCheng@users.noreply.github.com>
2021-04-16 21:12:40 +0800

    Update RegisterServlet.java

--- com/HuangZhiCheng/week3/RegisterServlet.java
+++ com/HuangZhiCheng/week3/RegisterServlet.java
@@ -1,37 +1,39 @@
-package com.HuangZhiCheng.week3;
+package com.HuangZhiCheng.week6;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class RegisterServlet extends HttpServlet {
+    Connection con = null;
     private static final long serialVersionUID = 3582264699128982106L;
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         /*
         request 对象接收来自用户端对服务器的指令，response对象从服务器输出信息到用户端
          */
-
-        String Username = request.getParameter("Username");//接收从网页收到的Username=**的值
-        String Password = request.getParameter("Password");
-        String Email = request.getParameter("Email");
-        String sex = request.getParameter("sex");
-        String birthday = request.getParameter("birthday");
-        PrintWriter writer = response.getWriter();
-        writer.println("<br/>Username:"+Username);//可识别html语言
-        writer.println("<br/>Password:"+Password);
-        writer.println("<br/>Email:"+Email);
-        writer.println("<br/>sex:"+sex);
-        writer.println("<br/>birthday:"+birthday);
-        writer.close();
-
+        this.doGet(request,response);
     }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String sql = "insert into usertbale values (?,?)";
+        PreparedStatement pre = null;
+        try {
+            pre = con.prepareStatement(sql);
+            pre.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        response.sendRedirect("index.jsp");
+    }
 
-
+    @Override
+    public void init() throws ServletException {
+         con =(Connection) getServletContext().getAttribute("con");
     }
 }
