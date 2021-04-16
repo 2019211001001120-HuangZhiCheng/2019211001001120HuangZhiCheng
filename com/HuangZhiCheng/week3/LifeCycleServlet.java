b9d2f901cdc1b604cfb5e55a94781e6311442f60
2019211001001120-HuangZhiCheng <80087154+2019211001001120-HuangZhiCheng@users.noreply.github.com>
2021-04-16 21:13:55 +0800

    Update LifeCycleServlet.java

--- com/HuangZhiCheng/week3/LifeCycleServlet.java
+++ com/HuangZhiCheng/week3/LifeCycleServlet.java
@@ -1,34 +1,62 @@
-package com.HuangZhiCheng.week3;
+package com.HuangZhiCheng.week6;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
 
-public class LifeCycleServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
+    private Connection con = null;
+    private static final long serialVersionUID = 6958143394670602948L;
 
-    public LifeCycleServlet(){
-        System.out.println("I am in constructer");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        PrintWriter writer = response.getWriter();
+        PreparedStatement pre=null;
+        String sql="select * from usertable where username=? and password=?";
+        try {
+            pre = con.prepareStatement(sql);
+            pre.setString(1,username);
+            pre.setString(2,password);
+            ResultSet resultSet = pre.executeQuery();
+            if(resultSet.next()){
+                request.setAttribute("id",resultSet.getInt("id"));
+                request.setAttribute("username",resultSet.getString("username"));
+                request.setAttribute("password",resultSet.getString("password"));
+                request.setAttribute("email",resultSet.getString("email"));
+                request.setAttribute("gender",resultSet.getString("gender"));
+                request.setAttribute("birthdate",resultSet.getDate("birthdate"));
+                request.getRequestDispatcher("userinfo.jsp").forward(request,response);
+            }else {
+                request.setAttribute("message","Usename or Password Error");
+                request.getRequestDispatcher("login.jsp").forward(request,response);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request,response);
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void init() throws ServletException {
-        System.out.println("I am in init()");
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        System.out.println("I am in servise-->doget");
-
-    }
-
-    @Override
-    public void destroy(){
-        System.out.println("I am in servise--->destroy");
+        con =(Connection) getServletContext().getAttribute("con");
     }
 }
