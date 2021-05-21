d88bdb6304b2a1a04f5071a1d42e67eac2fc5b08
2019211001001120-HuangZhiCheng <80087154+2019211001001120-HuangZhiCheng@users.noreply.github.com>
2021-05-21 10:26:49 +0800

    Add files via upload

--- com/HuangZhiCheng/dao/ProductDao.java
+++ com/HuangZhiCheng/dao/ProductDao.java
@@ -2,9 +2,8 @@ package com.HuangZhiCheng.dao;
 
 import com.HuangZhiCheng.model.Product;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDao implements  IProductDao{
@@ -41,13 +40,45 @@ public class ProductDao implements  IProductDao{
     }
 
     @Override
-    public Product findById(Integer productId, Connection con) {
-        return null;
+    public Product findById(Integer productId, Connection con) throws SQLException {
+        String queryString = "select * from Product where productId = ?";
+        PreparedStatement pt = con.prepareStatement(queryString);
+        pt.setInt(1,productId);
+        ResultSet rs = pt.executeQuery();
+        Product product = new Product();
+        while (rs.next()){
+            product.setProductId(rs.getInt("ProductId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductDescription(rs.getString("ProductDescription"));
+            product.setPrice(rs.getDouble("Price"));
+            product.setCategoryId(rs.getInt("CategoryId"));
+        }
+        System.out.println("Successful");
+        return product;
+
+
+
     }
 
     @Override
-    public List<Product> findByCategoryId(int categoryId, Connection con) {
-        return null;
+    public List<Product> findByCategoryId(int categoryId, Connection con) throws SQLException {
+        List<Product> list = new ArrayList<>();
+        String queryString = "select * from Product where categoryId = ?";
+        PreparedStatement pt = con.prepareStatement(queryString);
+        pt.setInt(1,categoryId);
+        ResultSet rs = pt.executeQuery();
+        while (rs.next()){
+            Product product = new Product();
+            product.setProductId(rs.getInt("ProductId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductDescription(rs.getString("ProductDescription"));
+            product.setPrice(rs.getDouble("Price"));
+            product.setCategoryId(rs.getInt("CategoryId"));
+            list.add(product);
+        }
+        System.out.println("Successful");
+        return list;
+
     }
 
     @Override
@@ -57,8 +88,21 @@ public class ProductDao implements  IProductDao{
 
     @Override
     public List<Product> findAll(Connection con) throws SQLException {
-
-        return null;
+        List<Product> list = new ArrayList<>();
+        String queryString = "select * from Product";
+        PreparedStatement pt = con.prepareStatement(queryString);
+        ResultSet rs = pt.executeQuery();
+        while (rs.next()){
+            Product product = new Product();
+            product.setProductId(rs.getInt("ProductId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductDescription(rs.getString("ProductDescription"));
+            product.setPrice(rs.getDouble("Price"));
+            product.setCategoryId(rs.getInt("CategoryId"));
+            list.add(product);
+        }
+        System.out.println("Successful");
+        return list;
     }
 
     @Override
@@ -69,5 +113,18 @@ public class ProductDao implements  IProductDao{
     @Override
     public List<Product> getPicture(Integer productId, Connection con) throws SQLException {
         return null;
+    }
+
+    public byte[] getPictureById(Integer productId,Connection con) throws SQLException {
+        byte[] imgBytes = null;
+        String sql = "select * from product where productId = ?";
+        PreparedStatement pt = con.prepareStatement(sql);
+        pt.setInt(1,productId);
+        ResultSet rs = pt.executeQuery();
+        while (rs.next()){
+            Blob blob = rs.getBlob("picture");
+            imgBytes = blob.getBytes(1,(int)blob.length());//get All
+        }
+        return imgBytes;
     }
 }
